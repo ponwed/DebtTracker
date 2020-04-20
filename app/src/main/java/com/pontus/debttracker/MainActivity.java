04c63cf190b337f338bac3ae95e5cc5fb3bacce8
Pontus <pontus.wed@gmail.com>
2020-04-20 15:45:04 +0200

    Added back toolbar to main activity

--- app/src/main/java/com/pontus/debttracker/MainActivity.java
+++ app/src/main/java/com/pontus/debttracker/MainActivity.java
@@ -1,6 +1,7 @@
 package com.pontus.debttracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -30,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //this.getSupportActionBar().hide(); //hide the title bar
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
 
         FloatingActionButton fabAddNew = findViewById(R.id.fab_new);
         fabAddNew.setOnClickListener(new View.OnClickListener()
