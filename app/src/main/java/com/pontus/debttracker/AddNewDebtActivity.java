f4b703f4dc3827cd857e6ba05b8b0c70a46498f4
Pontus <pontus.wed@gmail.com>
2020-04-20 12:22:01 +0200

    Populate spinner with currency codes

--- app/src/main/java/com/pontus/debttracker/AddNewDebtActivity.java
+++ app/src/main/java/com/pontus/debttracker/AddNewDebtActivity.java
@@ -8,20 +8,27 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Currency;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
+import java.util.Set;
 
 public class AddNewDebtActivity extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -35,6 +42,8 @@ public class AddNewDebtActivity extends AppCompatActivity
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
+
+        populateSpinner();
     }
 
     @Override
@@ -51,21 +60,65 @@ public class AddNewDebtActivity extends AppCompatActivity
         return true;
     }
 
+    // Modified function from https://stackoverflow.com/a/3537085/2195880
+    private static List<String> getAllCurrencyCodes()
+    {
+        Set<String> toret = new HashSet<String>();
+        Locale[] locs = Locale.getAvailableLocales();
+
+        for(Locale loc : locs) {
+            try {
+                Currency currency = Currency.getInstance( loc );
+
+                if ( currency != null ) {
+                    toret.add(currency.getCurrencyCode());
+                }
+            } catch(Exception exc)
+            {
+                // Locale not found
+            }
+        }
+
+        List <String> currencyList = new ArrayList<>(toret);
+        Collections.sort(currencyList);
+        return currencyList;
+    }
+
+    private String getCurrencyStringForLocale()
+    {
+        Locale defaultLocale = Locale.getDefault();
+        return Currency.getInstance(defaultLocale).getCurrencyCode();
+    }
+
+    private void populateSpinner()
+    {
+        List<String> spinnerArr = getAllCurrencyCodes();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_dropdown_item, spinnerArr
+        );
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner currencySpinner = findViewById(R.id.currency_spinner);
+        currencySpinner.setAdapter(adapter);
+        currencySpinner.setSelection(adapter.getPosition(getCurrencyStringForLocale()));
+    }
+
     public void onComposeAction(MenuItem mi)
     {
         TextInputLayout etName = findViewById(R.id.et_Name);
         TextInputLayout etDesc = findViewById(R.id.et_desc);
         TextInputLayout etSum = findViewById(R.id.et_sum);
+        Spinner currencySpinner = findViewById(R.id.currency_spinner);
 
         DateFormat dateFormat = DateFormat.getDateInstance();
         String date = dateFormat.format(new Date());
 
         Bundle extras = new Bundle();
 
-
         extras.putString("name", Objects.requireNonNull(etName.getEditText()).getText().toString());
         extras.putString("desc", Objects.requireNonNull(etDesc.getEditText()).getText().toString());
-        extras.putString("sum", Objects.requireNonNull(etSum.getEditText()).getText().toString());
+        extras.putString("sum", Objects.requireNonNull(etSum.getEditText()).getText().toString() + " " + currencySpinner.getSelectedItem().toString());
         extras.putString("date", date);
 
         Intent intent = new Intent();
